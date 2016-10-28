b058a4e83c081bbea9a5a1d482536caa61ca0c0e
Ron Oskam <roskam@oaisd.org>
2016-10-27 22:00:49 -0400

    Add project files.

---
+++ NurseryAssigner.Win/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace NurseryAssigner.Win
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.loadButton = new System.Windows.Forms.Button();
+      this.scrollPanel = new System.Windows.Forms.Panel();
+      this.scheduleTable = new System.Windows.Forms.TableLayoutPanel();
+      this.scrollPanel.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // loadButton
+      // 
+      this.loadButton.Location = new System.Drawing.Point(12, 10);
+      this.loadButton.Name = "loadButton";
+      this.loadButton.Size = new System.Drawing.Size(54, 21);
+      this.loadButton.TabIndex = 1;
+      this.loadButton.Text = "Load";
+      this.loadButton.UseVisualStyleBackColor = true;
+      this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+      // 
+      // scrollPanel
+      // 
+      this.scrollPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.scrollPanel.AutoScroll = true;
+      this.scrollPanel.Controls.Add(this.scheduleTable);
+      this.scrollPanel.Location = new System.Drawing.Point(0, 37);
+      this.scrollPanel.Name = "scrollPanel";
+      this.scrollPanel.Size = new System.Drawing.Size(712, 287);
+      this.scrollPanel.TabIndex = 2;
+      // 
+      // scheduleTable
+      // 
+      this.scheduleTable.AllowDrop = true;
+      this.scheduleTable.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.scheduleTable.AutoSize = true;
+      this.scheduleTable.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.scheduleTable.ColumnCount = 7;
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+      this.scheduleTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+      this.scheduleTable.Location = new System.Drawing.Point(0, 0);
+      this.scheduleTable.Name = "scheduleTable";
+      this.scheduleTable.RowCount = 1;
+      this.scheduleTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+      this.scheduleTable.Size = new System.Drawing.Size(715, 18);
+      this.scheduleTable.TabIndex = 1;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(712, 324);
+      this.Controls.Add(this.scrollPanel);
+      this.Controls.Add(this.loadButton);
+      this.Name = "MainForm";
+      this.Text = "Nursury Assigner";
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      this.scrollPanel.ResumeLayout(false);
+      this.scrollPanel.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+    private System.Windows.Forms.Button loadButton;
+    private System.Windows.Forms.Panel scrollPanel;
+    private System.Windows.Forms.TableLayoutPanel scheduleTable;
+  }
+}
+
---
+++ NurseryAssigner.Win/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NurseryAssigner.Data;
+
+namespace NurseryAssigner.Win
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void loadButton_Click(object sender, EventArgs e)
+    {
+      var startDate = new DateTime(2016, 7, 1);
+      var endDate = new DateTime(2016, 12, 31);
+      var db = new NurseryAssignerEntities();
+      var days = db.ScheduledDays.Where(s => s.Date >= startDate && s.Date <= endDate)
+        .OrderBy(s => s.Date).ThenBy(s => s.AMPM).ToList();
+
+      scheduleTable.SuspendLayout();
+      int row = 1;
+      foreach (var day in days)
+      {
+        scheduleTable.RowCount++;
+        var newRowStyle = new RowStyle(SizeType.Absolute, 20);
+        scheduleTable.RowStyles.Add(newRowStyle);
+        addLabel(day.Date.ToString("MMM d"), 0, row);
+        addLabel(day.AMPM, 1, row);
+        foreach (var schedule in day.AttendantSchedules.OrderBy(s => s.Position))
+          addPerson(schedule, Convert.ToInt32(schedule.Position) + 1, row);
+
+        row++;
+      }
+      scheduleTable.ResumeLayout();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+      var font = new Font(scheduleTable.Font, FontStyle.Bold);
+
+      addLabel("Date", 0, 0, font);
+      addLabel("A/P", 1, 0, font);
+      addLabel("First Adult", 2, 0, font);
+      addLabel("Second Adult", 3, 0, font);
+      addLabel("Third Adult", 4, 0, font);
+      addLabel("Middle School", 5, 0, font);
+      addLabel("Elementary", 6, 0, font);
+    }
+
+    private void addLabel(string text, int column, int row, Font font = null)
+    {
+      var label = new Label();
+      label.Text = text;
+      label.Dock = DockStyle.Fill;
+      label.TextAlign = ContentAlignment.MiddleLeft;
+      if (font != null)
+        label.Font = font;
+      scheduleTable.Controls.Add(label, column, row);
+    }
+
+    private void addPerson(AttendantSchedule schedule, int column, int row)
+    {
+      var label = new Label();
+      label.Text = schedule.Attendant.FirstName + " " + schedule.Attendant.LastName;
+      label.Tag = schedule;
+      label.Dock = DockStyle.Fill;
+      label.TextAlign = ContentAlignment.MiddleLeft;
+      label.AllowDrop = true;
+      label.MouseDown += Label_MouseDown;
+      label.DragEnter += Label_DragEnter;
+      label.DragDrop += Label_DragDrop;
+      scheduleTable.Controls.Add(label, column, row);
+    }
+
+    private void Label_DragDrop(object sender, DragEventArgs e)
+    {    
+      var source = (Label)e.Data.GetData(typeof(Label));
+      var destination = (Label)sender;
+
+      if (source.Text != destination.Text 
+        && MessageBox.Show("Are you sure you want to swap " + source.Text + " with " + destination.Text + "?", Application.ProductName, MessageBoxButtons.OKCancel) == DialogResult.OK)
+      {
+        var swap = source.Text;
+        source.Text = destination.Text;
+        destination.Text = swap;
+
+        var tagSwap = source.Tag;
+        source.Tag = destination.Tag;
+        destination.Tag = tagSwap;
+
+        var destSched = (AttendantSchedule)destination.Tag;
+        SetSelection(destSched.AttendantID);
+        destination.BackColor = Color.HotPink;
+      }
+    }
+
+    private void Label_DragEnter(object sender, DragEventArgs e)
+    {
+      e.Effect = DragDropEffects.Move;
+    }
+
+    private void Label_MouseDown(object sender, MouseEventArgs e)
+    {
+      var label = (Label)sender;      
+      var currentSched = (AttendantSchedule)(((Label)sender).Tag);
+
+      SetSelection(currentSched.AttendantID);
+      label.BackColor = Color.HotPink;
+
+      label.DoDragDrop(label, DragDropEffects.Move);
+    }
+
+    private void SetSelection(long? attendantID)
+    {
+      foreach (var item in scheduleTable.Controls)
+      {
+        if (item is Label && ((Label)item).Tag != null)
+        {
+          var label = (Label)item;
+          var itemSched = (AttendantSchedule)label.Tag;
+          if (!attendantID.HasValue || itemSched.AttendantID != attendantID.Value)
+            label.BackColor = Color.Transparent;
+          else
+            label.BackColor = Color.Pink;
+        }
+      }
+    }
+  }
+}
