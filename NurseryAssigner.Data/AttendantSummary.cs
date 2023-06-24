149d5dfdac8c7968b656b1bc14da6a3a0738950d
Ron Oskam <roskam@oaisd.org>
2023-06-24 19:49:31 -0400

    report grouping fix

--- NurseryAssigner.Data/AttendantSummary.cs
+++ NurseryAssigner.Data/AttendantSummary.cs
@@ -35,8 +35,7 @@ namespace NurseryAssigner.Data
        var schedule = db.AttendantSchedules.Include("Attendant").Include("Service").Where(s => s.Service.Date >= startDate && s.Service.Date <= endDate).OrderBy(s => s.AttendantID).ToList()
         .Select(s => new AttendantSummary(db)
         {
-          //HARD CODING ALERT!!  Have to manually put Boerman kids together because there are 2 families of them
-          GroupID = (s.AttendantID == 179 || s.AttendantID == 192 ? "BoermanRS" : s.Attendant.LastName),
+          GroupID = s.Attendant.LastName,
           Name = s.Attendant.FirstName + " " + s.Attendant.LastName,
           Day = s.Service.Date,
           AMPM = s.Service.AMPM
